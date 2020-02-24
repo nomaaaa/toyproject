3346ad287618b5636090f7398a83786776d8591d
root <root@localhost.localdomain>
2020-02-24 02:45:47 -0500

    python3 로 문법 수정

--- rank_catch.py
+++ rank_catch.py
@@ -9,31 +9,42 @@ class default():
 
     def run(self):
         now_time = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-        max_page = 40 
-        keyword_ko = '전기면도기'
-        keyword_en = urllib.quote(keyword_ko)
-        store_name = '어니스트원'
+        max_page = 30 
+        keyword_ko = '마블 케이스'
+        keyword_en = urllib.parse.quote(keyword_ko)
+        store_name = '유니폰'
+        count = 0
         
         
         for page in range(1,max_page,1):
-            url='https://search.shopping.naver.com/search/all.nhn?origQuery='+'%s' % keyword_en+'&pagingIndex='+'%s' % page+'&pagingSize=40&viewType=list&sort=rel&frm=NVSHPAG&query=%EC%A0%84%EA%B8%B0%EB%A9%B4%EB%8F%84%EA%B8%B0' 
+            count +=1
+            print(count)
+
+            url='https://search.shopping.naver.com/search/all.nhn?origQuery='+'%s' % keyword_en+'&pagingIndex='+'%s' % page +'&pagingSize=40&viewType=list&sort=rel&frm=NVSHPAG&query=%EB%A7%88%EB%B8%94%20%EC%BC%80%EC%9D%B4%EC%8A%A4'
 
             payload = {'key1': 'value1', 'key2': 'value2'}
             r = requests.post(url, data=payload)
             result = r.text
-            enc_result = result.encode('utf8')
-            if enc_result.find('%s' % store_name) > -1:
-                print '%s 키워드: %s page : %s\n' % (now_time,keyword_ko,page)
+            #enc_result = result.encode('utf8')
+            #enc_result = enc_result.decode()
+            #if enc_result.find('%s' % store_name) > -1:
+            if result.find('%s' % store_name) > -1:
+                print('%s 키워드: %s page : %s\n' % (now_time,keyword_ko,page))
                 file=open('ranking.txt','a')
+                print('rankin.txt make success')
                 file.write('%s 키워드: %s page : %s\n' % (now_time,keyword_ko,page))
                 file.close()
 def main():
-    DF = default()
-    DF.run()
+    try:
+        DF = default()
+        DF.run()
+    except:
+        import traceback
+        traceback.print_exc()
 
 if __name__ == "__main__":
     stime = time.time()
     main()
     etime = time.time()
-    print round(etime-stime,3),' eclapsed'
+    print(round(etime-stime,3),' eclapsed')
 
